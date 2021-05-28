e6f5a269387f13a89c082972bad0d16bd6bc9fc1
Miguel Ricaño <mricanho@gmail.com>
2021-05-27 20:27:37 -0500

    Fix eslint errors

--- src/components/display.js
+++ src/components/display.js
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types';
 const Display = (props) => {
   const { result } = props;
   return (
-    <h1 classname="display-value">
+    <h1 className="display-value">
       {result}
     </h1>
-  )
+  );
 };
 
-Display.protoTypes = { 
-  result: ProtoTypes.string,
+Display.propTypes = {
+  result: PropTypes.string,
 };
 
 Display.defaultProps = {
-  result: "0",
+  result: '0',
 };
 
 export default Display;
