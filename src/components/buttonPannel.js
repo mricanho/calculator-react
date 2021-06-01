d42e9635316388665ec0e29b711c776b3bd2eaf8
Miguel Ricaño <mricanho@gmail.com>
2021-06-01 17:36:03 -0500

    Add clickHandler to buttonPannel.js

--- src/components/buttonPannel.js
+++ src/components/buttonPannel.js
@@ -2,38 +2,47 @@ import React, { Fragment } from 'react';
 import Button from './button';
 import '../index.css';
 
-const ButtonPanel = () => (
-  <>
-    <div className="d-grid">
-      <Button buttonName="AC" type="sp" />
-      <Button buttonName="+/-" type="sp" />
-      <Button buttonName="%" type="sp" />
-      <Button buttonName="/" type="op" />
-    </div>
-    <div className="d-grid">
-      <Button buttonName="7" type="num" />
-      <Button buttonName="8" type="num" />
-      <Button buttonName="9" type="num" />
-      <Button buttonName="X" type="op" />
-    </div>
-    <div className="d-grid">
-      <Button buttonName="4" type="num" />
-      <Button buttonName="5" type="num" />
-      <Button buttonName="6" type="num" />
-      <Button buttonName="-" type="op" />
-    </div>
-    <div className="d-grid">
-      <Button buttonName="1" type="num" />
-      <Button buttonName="2" type="num" />
-      <Button buttonName="3" type="num" />
-      <Button buttonName="+" type="op" />
-    </div>
-    <div className="d-grid-last">
-      <Button buttonName="0" type="num" />
-      <Button buttonName="." type="num" />
-      <Button buttonName="=" type="op" />
-    </div>
-  </>
-);
+const ButtonPanel = ({ clickHandler }) => {
+  const clickHandle = (buttonName) => {
+    clickHandler(buttonName);
+  };
+  return (
+    <>
+      <div className="d-grid">
+        <Button buttonName="AC" handlerClick={clickHandle} type="sp" />
+        <Button buttonName="+/-" handlerClick={clickHandle} type="sp" />
+        <Button buttonName="%" handlerClick={clickHandle} type="sp" />
+        <Button buttonName="/" handlerClick={clickHandle} type="op" />
+      </div>
+      <div className="d-grid">
+        <Button buttonName="7" handlerClick={clickHandle} type="num" />
+        <Button buttonName="8" handlerClick={clickHandle} type="num" />
+        <Button buttonName="9" handlerClick={clickHandle} type="num" />
+        <Button buttonName="X" handlerClick={clickHandle} type="op" />
+      </div>
+      <div className="d-grid">
+        <Button buttonName="4" handlerClick={clickHandle} type="num" />
+        <Button buttonName="5" handlerClick={clickHandle} type="num" />
+        <Button buttonName="6" handlerClick={clickHandle} type="num" />
+        <Button buttonName="-" handlerClick={clickHandle} type="op" />
+      </div>
+      <div className="d-grid">
+        <Button buttonName="1" handlerClick={clickHandle} type="num" />
+        <Button buttonName="2" handlerClick={clickHandle} type="num" />
+        <Button buttonName="3" handlerClick={clickHandle} type="num" />
+        <Button buttonName="+" handlerClick={clickHandle} type="op" />
+      </div>
+      <div className="d-grid-last">
+        <Button buttonName="0" handlerClick={clickHandle} type="num" />
+        <Button buttonName="." handlerClick={clickHandle} type="num" />
+        <Button buttonName="=" handlerClick={clickHandle} type="op" />
+
+      </div>
+    </>
+  );
+};
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
