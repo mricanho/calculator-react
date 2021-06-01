7eefd7edb6225843dcd28480c7a110ea464b1bfa
Miguel Ricaño <mricanho@gmail.com>
2021-06-01 17:34:07 -0500

    Add handlerClick to button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const { buttonName, type } = props;
-
+const Button = ({ buttonName, handlerClick, type }) => {
+  const handleClick = () => handlerClick(buttonName);
+  let color = 'teal';
+  if (type === 'sp') {
+    color = 'purple';
+  } if (type === 'op') {
+    color = 'blue';
+  }
   return (
-    <div className={type}>{buttonName}</div>
+    <button type="button" className={`ui button ${color}`} onClick={handleClick}>{buttonName}</button>
   );
 };
 
 Button.propTypes = {
   buttonName: PropTypes.string,
+  handlerClick: PropTypes.func.isRequired,
   type: PropTypes.string,
 };
 
