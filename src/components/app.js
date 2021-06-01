65fbf0b5b69e0b0ab700b313b68a5e1fcff9b61e
Miguel Ricaño <mricanho@gmail.com>
2021-06-01 17:33:03 -0500

    Add handleClick to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,39 @@
-/* eslint-disable no-unused-vars */
 import React, { Fragment } from 'react';
 import Display from './display';
 import ButtonPannel from './buttonPannel';
 import Calculate from '../logic/calculate';
 
-const result = '0';
-const App = () => (
-  <>
-    <Display result={result} />
-    <ButtonPannel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
+  render() {
+    const handleClick = (buttonName) => {
+      const result = calculate(this.state, buttonName);
+      this.setState({ ...result });
+    };
+    const { next, total } = this.state;
+
+    return (
+      <div className="ui grid container">
+        <div className="sixteen wide mobile eight wide tablet ten wide computer column">
+          <Display result={(next && next.toString()) || (total && total.toString())} />
+          <ButtonPanel clickHandler={(e) => handleClick(e)} />
+        </div>
+        <footer className="footer container center aligned inverted ui header segment">
+          {'Created by '}
+          <a href="https://miguelricano.me" className="ui header teal">
+            Miguel Ricaño
+          </a>
+        </footer>
+      </div>
+    );
+  }
+}
 export default App;
