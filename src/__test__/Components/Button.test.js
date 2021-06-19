ba5c3f3b9a50fad7d50a9583d2373d4911014d58
Miguel Ricaño <mricanho@gmail.com>
2021-06-19 13:05:16 -0500

    Add button snapshot test

---
+++ src/__test__/Components/Button.test.js
@@ -0,0 +1,9 @@
+  
+import { create } from 'react-test-renderer';
+
+import Button from '../../components/Button';
+
+it('Should renders correctly', () => {
+  const elem = create(<Button buttonName="button" handlerClick={() => 'this is a button'} type="sp" />);
+  expect(elem).toMatchSnapshot();
+});
