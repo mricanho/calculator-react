1de0ab8943c3fe37c29bf007f5fa9bf51ef09235
Miguel Ricaño <mricanho@gmail.com>
2021-06-19 09:55:58 -0500

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h3>Welcome to our page!</h3>
+    <p>
+      Lorem ipsum odor amet, consectetuer adipiscing elit.
+      Nisi mauris nullam accumsan sem condimentum quis penatibus ligula.
+      Curae maecenas himenaeos felis nascetur platea placerat nam.
+      Venenatis litora senectus conubia diam lectus nulla vitae pharetra.
+      Porttitor nec vivamus justo volutpat libero, mattis tempor varius urna.
+      Ligula imperdiet inceptos accumsan bibendum elementum viverra vulputate ultrices malesuada.
+      Ipsum elementum nascetur; tempus mus maecenas integer elit senectus hac.
+    </p>
+    <p>
+      Vel adipiscing ultrices ligula mattis eu dis tortor malesuada habitasse.
+      Rutrum mauris curae; proin per nunc duis dolor vestibulum.
+      Dignissim praesent vivamus feugiat porttitor pellentesque malesuada cras conubia.
+      Nulla dignissim magnis curabitur finibus sapien penatibus aenean.
+      Eget lacinia ultrices neque class; sapien tellus a.
+      Maximus torquent consequat mattis maecenas pulvinar; amet facilisis.
+      Facilisis vitae netus habitant semper tempus duis, feugiat tellus.
+      Eleifend euismod volutpat accumsan hendrerit suscipit ad.
+    </p>
+  </>
+);
+
+export default Home;
